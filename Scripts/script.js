ba6545622a43efe4ef4f98b8b31ae6997e5fc6ec
zero-one-two <mgill2909@gmail.com>
2020-03-27 19:00:00 -0700

    player walking setup

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,11 +1,22 @@
-//Part A ---------Generates buttons and disables onclick event----------
+//Part 0 ------------------Constants & Variables------------------------
 let objArray;
+let lastStride = "R";
+let player = {x: -10};
+let stopMoveFlag = false;
+//Part 0 ---------------------------------------------------------------
 
+//Part A ---------Generates buttons and disables onclick event----------
 function controller(btn) {
     return function buttonClick() {
         btn.setAttribute('class', "dead");
         btn.disabled = true;
         btn.onclick = "";
+
+        if (lastStride == "R") {
+            strideL();
+        } else {
+            strideR();
+        }
     }
 }
 function buttonMaker() {
@@ -22,3 +33,49 @@ function buttonMaker() {
 }
 buttonMaker();
 //Part A ---------------------------------------------------------------
+
+//Part B ---------------Controls Player Walking Animation---------------
+function move() {
+    if (stopMoveFlag) {
+        return;
+    }
+    let newX = player.x + 0.02;
+    player.x = newX;
+    document.getElementById("player").style.left = newX + "vw";
+    setTimeout(move, 2);
+}
+
+function strideL() {
+    stopMoveFlag = false;
+    lastStride = "L";
+    document.getElementById("player").style.top = "47.3vh";
+    document.getElementById("player").src = "Images/stride1.gif";
+    setTimeout(move, 2);
+    setTimeout(function () {
+        stopMoveFlag = true;
+    }, 2450);
+    setTimeout(function () {
+        document.getElementById("player").style.top = "47vh";
+        document.getElementById("player").src = "Images/stop1.png";
+    }, 2500);
+    
+}
+function strideR() {
+    stopMoveFlag = false;
+    lastStride = "R";
+    document.getElementById("player").style.top = "47.3vh";
+    document.getElementById("player").src = "Images/stride2.gif";
+    move();
+    setTimeout(function () {
+        stopMoveFlag = true;
+    }, 2400);
+    setTimeout(function () {
+        document.getElementById("player").style.top = "47vh";
+        document.getElementById("player").src = "Images/stop2.png";
+    }, 2400);
+}
+//Part B ---------------------------------------------------------------
+
+//Part C ----------------
+
+//Part C ---------------------------------------------------------------
