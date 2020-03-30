97ec0a4b1ef7e6e95e58f11ca28bfffdc8bbdae2
zero-one-two <mgill2909@gmail.com>
2020-03-30 02:10:49 -0700

    guess letter functionality added

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,41 +1,66 @@
 //Part 0 ------------------Constants & Variables------------------------
 let objArray;
 let lastStride = "R"; //The last leg the player swung to walk, maintains consistent walking animation.
-let player = {x: -5}; //Players location on the screen
+let player = {x: -5, textX: 7, bubbleX: -4}; //Players location on the screen
+
 let stopMoveFlag = false; //Controls player motion
-let stepCount = 0; //At 3 steps, player reaches computer and the game screen appears on a delay.
+let stepCount = 1; //At 3 steps, player reaches computer and the game screen appears on a delay.
+let storeWord = "HELLO"; //Stores the word input by the firebase.
+let wordArray = []; //Stores the word as dashes at each index that get replaced with correctly guessed letters.
+
 //Part 0 ---------------------------------------------------------------
 
+//Part ! -------------------Game Start Function-------------------------
+function startGame() {
+    document.getElementById("thought").style.visibility = "visible";
+    document.getElementById("quest").style.visibility = "visible";
+    document.getElementById("dash").style.visibility = "visible";
+    buttonMaker();
+    pullWord();
+    createDash();
+}
+//Part ! ---------------------------------------------------------------
+
 //Part A ---------Generates buttons and disables onclick event----------
-function controller(btn) {
+function controller(btn, letter) {
     return function buttonClick() {
         btn.setAttribute('class', "dead");
         btn.disabled = true;
         btn.onclick = "";
-
+        updateDash(letter);
+        if (stepCount == 3) {
+            setTimeout(openScreen,3000);
+            stepCount++;
+        }
+        if (stepCount > 4) {
+            return;
+        }
         if (lastStride == "R") {
             strideL();
         } else if (lastStride == "L") {
             strideR();
         }
-        if (stepCount == 3) {
-            setTimeout(openScreen,3000);
-        }
+        
     }
 }
+
+function Button(i) {
+    this.btn = document.createElement("BUTTON");
+    this.btn.setAttribute('id', String.fromCharCode(65 + i));
+    this.letter = String.fromCharCode(65 + i);
+    this.btn.setAttribute('class', "btnz");
+    this.btn.onclick = controller(this.btn, this.letter);
+    this.btn.innerHTML = "" + String.fromCharCode(65 + i);
+    document.getElementById("buttons").appendChild(this.btn);
+}
+
 function buttonMaker() {
     let i = 0;
     let n = 26
     for (i; i < n; i++) {
-        btn = document.createElement("BUTTON");
-        btn.setAttribute('id', String.fromCharCode(65 + i));
-        btn.setAttribute('class', "btnz");
-        btn.onclick = controller(this.btn);
-        btn.innerHTML = "" + String.fromCharCode(65 + i);                    
-        document.getElementById("buttons").appendChild(btn);
+        let t = new Button(i);
     }
 }
-buttonMaker();
 //Part A ---------------------------------------------------------------
 
 //Part B ---------------Controls Player Walking Animation---------------
@@ -43,9 +68,14 @@ function move() {
     if (stopMoveFlag) {
         return;
     }
-    let newX = player.x + 0.01;
-    player.x = newX;
-    document.getElementById("player").style.left = newX + "vw";
+    player.x += 0.01;
+    player.bubbleX += 0.01;
+    player.textX += 0.01;
+
+    document.getElementById("player").style.left = player.x + "vw";
+    document.getElementById("thought").style.left = player.bubbleX + "vw";
+    document.getElementById("quest").style.left = player.textX + "vw";
+    document.getElementById("dash").style.left = player.textX + "vw";
     setTimeout(move, 2);
 }
 
@@ -83,17 +113,54 @@ function strideR() {
 //Part B ---------------------------------------------------------------
 
 //Part C ----------------Word Generator and Input Handler---------------
+//Opens the command window for when player reaches the computer.
 function openScreen () {
     document.getElementById("game").style.visibility = "visible";
     document.getElementById("box").style.visibility = "visible";
 }
 
+//Not yet working. Pulls word from firebase.
+function pullWord () {
+    return 0;
+}
 
+//Takes word input and creates dashed lines of equal size.
+function createDash() {
+    let i;
+    for (i = 0; i < storeWord.length; i++) {
+        wordArray[i] = "- ";
+        document.getElementById("dash").innerHTML += wordArray[i];
+    }
+}
+
+//Updates the dashes to reveal correctly guessed letter.
+function updateDash(letter) {
+    document.getElementById("dash").innerHTML = "";
+    let i;
+    for (i = 0; i < storeWord.length; i++) {
+        if (storeWord[i] == letter) {
+            wordArray[i] = letter + " ";
+        }
+        document.getElementById("dash").innerHTML += wordArray[i];
+    }
+}
+
+//Not yet working, uses enter key to accept password input.
+function enterPassword(ele) {
+    if (event.key  === 'Enter') {
+        if (ele == storeWord) {
+            teleport ();
+        }
+    }
+}
 //Part C ---------------------------------------------------------------
 
 //Part D -----------------------------End Game--------------------------
 //If the player wins, they get teleported into the ship.
 function teleport () {
+    document.getElementById("thought").style.visibility = "hidden";
+    document.getElementById("quest").style.visibility = "hidden";
+    document.getElementById("dash").style.visibility = "hidden";
     document.getElementById("port").style.visibility = "visible";
     document.getElementById("port").src= "Images/portal.gif";
     document.getElementById("game").style.visibility = "hidden";
