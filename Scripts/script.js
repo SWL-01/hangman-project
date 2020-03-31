2c33a4f6b5109f3edf1944a7fba09b114fa4779f
Siwoon Lim <mongle0629@gmail.com>
2020-03-30 20:27:36 -0700

    <firebase updated>

--- Scripts/script.js
+++ Scripts/script.js
@@ -9,6 +9,17 @@ let storeWord = "HELLO"; //Stores the word input by the firebase.
 let wordArray = []; //Stores the word as dashes at each index that get replaced with correctly guessed letters.
 
 //Part 0 ---------------------------------------------------------------
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAdwk7EKfxs9ehMNXUCjUrnmKg7a5H6Yis",
+    authDomain: "webdevproject-4929d.firebaseapp.com",
+    databaseURL: "https://webdevproject-4929d.firebaseio.com",
+    projectId: "webdevproject-4929d",
+    storageBucket: "webdevproject-4929d.appspot.com",
+    messagingSenderId: "138454634948",
+    appId: "1:138454634948:web:0c0749f1dfb334b7ba1491",
+    measurementId: "G-CCV5C1JR0J"
+  };
 
 //Part ! -------------------Game Start Function-------------------------
 function startGame() {
